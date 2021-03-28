0c3ac343c06e8924f653ddfc5307276e925c9fd5
Valentino <duvalvalentino@gmail.com>
2021-03-28 21:06:08 +0100

    Update
    Move to use functions starting

---
+++ CShareFuncs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace cShare
+{
+    public class CShareFuncs
+    {
+        public String saveLocation()
+        {
+            string SaveLoc = Path.GetRandomFileName();
+            return SaveLoc;
+        }
+    }
+
+    public class ImgObject
+    {
+        public string objFileName { get; set; }
+        public Bitmap objCapImg { get; set; }
+        public ImgObject(string fileName, Bitmap capImg)
+        {
+
+            objFileName = fileName;
+            objCapImg = capImg;
+
+
+        }
+    }
+}
---
+++ MyUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cShare
+{
+    class MyUnitTests
+    {
+    }
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
